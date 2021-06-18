56db6826cbc65c6e7b743a00bf18199db447cb15
NRAlejandro <nicolas.ramirez.alejandro@gmail.com>
2021-06-17 22:48:02 -0300

    Close #7

--- src/main/java/com/github/jmpala/Imageboard/controller/PostController.java
+++ src/main/java/com/github/jmpala/Imageboard/controller/PostController.java
@@ -1,7 +1,7 @@
 package com.github.jmpala.Imageboard.controller;
 
-import com.github.jmpala.Imageboard.dao.Post;
 import com.github.jmpala.Imageboard.dto.PostDto;
+import com.github.jmpala.Imageboard.service.CategoryService;
 import com.github.jmpala.Imageboard.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,28 +9,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class PostController {
 
     private PostService postService;
+    private CategoryService categoryService;
 
     @Autowired
-    public PostController(PostService postService) {
+    public PostController(PostService postService, CategoryService categoryService) {
         this.postService = postService;
+        this.categoryService = categoryService;
     }
 
-    @RequestMapping("/boards/{category}/{post}")
-    public String showSelectedPost(@PathVariable("category") String category,
-                                   @PathVariable("post") String post,
+    @RequestMapping("/post/{post_id}")
+    public String showSelectedPost(@PathVariable("post_id") String post_id,
                                    Model model) {
 
-        Optional<PostDto> oPostDto = postService.findById(Long.valueOf(post));
+        Optional<PostDto> oPostDto = postService.findById(Long.valueOf(post_id));
+        model.addAttribute("categories", categoryService.findAll());
         model.addAttribute("post", oPostDto.get());
 
         return "post.html";
     }
+
 
 }
