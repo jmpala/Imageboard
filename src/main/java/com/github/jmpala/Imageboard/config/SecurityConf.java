2319ff6f521728ae370dbb033ebccb4fdb0d2b80
jmpala <pala-o-92@hotmail.com>
2021-06-19 02:13:25 +0200

    close #12

--- src/main/java/com/github/jmpala/Imageboard/config/SecurityConf.java
+++ src/main/java/com/github/jmpala/Imageboard/config/SecurityConf.java
@@ -1,13 +1,31 @@
 package com.github.jmpala.Imageboard.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+import java.lang.reflect.Executable;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConf extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT users.username, roles.role FROM users INNER JOIN roles ON users.id = roles.id WHERE users.username = ?");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,4 +41,5 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
                 .logout()
                     .permitAll();
     }
+
 }
